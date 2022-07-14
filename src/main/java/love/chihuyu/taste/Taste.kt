4360eb301303e7dc970fc54bea47e2f0e721f934
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-07-14 18:20:03 +0900

    Initial commit

---
+++ src/main/java/love/chihuyu/taste/Taste.kt
@@ -0,0 +1,13 @@
+package love.chihuyu.taste
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Taste : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
